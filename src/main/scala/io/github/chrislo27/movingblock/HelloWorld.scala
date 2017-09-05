e13e4c367cb7e9a1a8fd066d4ba87b2b736bfa83
chrislo27 <kinchrislo@gmail.com>
2017-09-04 21:17:07 -0700

    Add cool things

--- src/main/scala/io/github/chrislo27/movingblock/HelloWorld.scala
+++
@@ -1,7 +0,0 @@
-package io.github.chrislo27.movingblock
-
-object HelloWorld extends App {
-
-	println("Hello world!")
-
-}
---
+++ src/main/scala/io/github/chrislo27/movingblock/MovingBlockApp.scala
@@ -0,0 +1,43 @@
+package io.github.chrislo27.movingblock
+
+import javafx.application.Application
+import javafx.stage.{Screen, Stage}
+
+import com.sun.javafx.font.PrismFontFactory
+
+import scala.collection.JavaConverters._
+
+
+class MovingBlockApp extends Application {
+
+  private def handleHDPI(): Unit = {
+    val fontSize = getParameters.getNamed.asScala.get("fontSize").map(_.toDouble)
+    if (util.Properties.isLinux || fontSize.isDefined) {
+      val systemFontSize = Screen.getPrimary.getDpi match {
+        case -1 => fontSize
+        case screenDpi => Some(screenDpi / 6) // 12 points
+      }
+
+      systemFontSize foreach { systemFontSize =>
+        val clazz = classOf[PrismFontFactory].getDeclaredField("systemFontSize")
+        clazz.setAccessible(true)
+        clazz.setFloat(null, systemFontSize.toFloat)
+      }
+    }
+  }
+
+  override def init(): Unit = {
+    handleHDPI()
+  }
+
+  override def start(primaryStage: Stage) {
+    primaryStage.show()
+  }
+
+  object MovingBlockApp {
+
+    def main(args: Array[String]): Unit = Application.launch(classOf[MovingBlockApp])
+
+  }
+
+}
